f230c94949431fec92cbc18729d37434e5a365f3
karsonshin <karson.shin@outlook.com>
2025-10-14 12:53:41 -0700

    AdminHomePage

--- src/application/AdminHomePage.java
+++ src/application/AdminHomePage.java
@@ -15,23 +15,23 @@ import javafx.geometry.Pos;
 public class AdminHomePage {
     private Stage stage;
     private String userName;
-    
+
     //constructor
     public AdminHomePage(Stage stage, String userName) {
         this.stage = stage;
         this.userName = userName;
     }
-    
+
     //create the scene
     public Scene createScene() {
         VBox layout = new VBox(20);
         layout.setAlignment(Pos.CENTER);
         layout.setPadding(new Insets(20));
-        
+
         //label to display the welcome message for the admin
         Label adminLabel = new Label("Hello, Admin " + userName + "!");
         adminLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
-        
+
         //discussion board button
         Button discussionBoardBtn = new Button("Discussion Board");
         discussionBoardBtn.setPrefWidth(200);
@@ -39,11 +39,11 @@ public class AdminHomePage {
             DiscussionBoardPage dbPage = new DiscussionBoardPage(stage, userName, "admin");
             stage.setScene(dbPage.createScene());
         });
-        
+
         layout.getChildren().addAll(adminLabel, discussionBoardBtn);
         return new Scene(layout, 800, 400);
     }
-    
+
 	/**
      * Displays the admin page in the provided primary stage.
      * @param primaryStage The primary stage where the scene will be displayed.
